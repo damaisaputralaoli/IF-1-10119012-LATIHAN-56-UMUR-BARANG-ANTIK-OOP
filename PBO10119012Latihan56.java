22e14304fa3e1f7b5b7f7b0d56c9aa76056bdcce
damaisaputralaoli <73173498+damaisaputralaoli@users.noreply.github.com>
2020-11-22 10:45:00 +0700

    Add files via upload

---
+++ PBO10119012Latihan56.java
@@ -0,0 +1,20 @@
+
+package pbo10119012latihan56;
+
+/**
+ *   NAMA   : DAMAI SAPUTRA LAOLI
+ *   KELAS  : IF-1
+ *   NIM    : 10119012
+ * @author damai
+ */
+public class PBO10119012Latihan56 {
+
+    
+    public static void main(String[] args) {
+        Radio radio1 = new Radio(234);
+        radio1.setName("Radio AM");
+        System.out.println("Nama barang antik : " + radio1.getName());
+        radio1.tampilUmur();
+    }
+    
+}
